cbce93c2dd4268cf4b72d59db1fbfa06b3ba8aac
Gowthami889 <142968924+Gowthami889@users.noreply.github.com>
2023-08-23 12:28:13 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,10 @@
 import streamlit
-import pandas as pd
-
+import pandas
 streamlit.title('welcome to my first app 🥣')
 streamlit.header('Hello 🥑')
 streamlit.text('welcome 🐔')
 streamlit.text('to My ')
 streamlit.text('page! 🍞')
 streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-my_fruite_list=pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
+my_fruite_list=pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 streamlit.dataframe(my_fruite_list)
